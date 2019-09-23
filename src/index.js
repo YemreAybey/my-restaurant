bde96050fa71ecb50a763e90845c7accf70fed15
Yemre <aybey77@gmail.com>
2019-09-23 18:11:39 +0300

    Menu Page is Done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import homeLoad from "./modules/home";
-import loadNavBar from "./modules/navbar";
-import { brotliDecompressSync } from "zlib";
+import homeLoad from './modules/home';
+import loadNavBar from './modules/navbar';
+import loadMenu from './modules/menu';
 
 const content = document.getElementById('content');
 function render(e) {
   content.innerHTML = "";
   content.appendChild(page);
-  if (e.target.id == 'home')
-  content.appendChild(home);
+  const body = document.querySelector('body');
+  const navs = document.querySelectorAll('.right-align>span');
+  const brand = document.querySelector('.brand');
+  if (e.target.id == 'home') {
+    body.style.backgroundImage = "url('imgs/about.jpg')";
+    content.appendChild(home);
+    navs.forEach(n => n.style.color = 'rgb(238, 212, 109)');
+    brand.style.color = 'rgb(253, 255, 116)';
+  }
+  else if (e.target.id == 'menu') {
+    body.style.backgroundImage = 'none';
+    content.appendChild(menu);
+    navs.forEach(n => n.style.color = 'black');
+    brand.style.color = 'black';
+  } else {
+    
+  }
 }
 
 
 const page = loadNavBar();
 const home = homeLoad();
+const menu = loadMenu();
 // const menu = menuLoad();
 // const contact = contactLoad();
 
